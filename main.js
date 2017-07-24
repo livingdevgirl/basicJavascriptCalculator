cf38e4203b3aad8e04415e85b5f605048dca3338
Emma Dean <emmadeannow@gmail.com>
2017-07-24 03:13:53 -0400

    fixed filepaths

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+let buttons = document.querySelectorAll('button');
+let solutionWindow = document.getElementById('solutionWindow');
+let equal = document.getElementById('equal');
+let clear = document.getElementById('clearButton')
+
 
 function calculate(){
-  solutionWindow.value = eval(solutionWindow.value);
-}
+  (solutionWindow.value = eval(solutionWindow.value))};
 
-let buttons = document.querySelectorAll('button');
-let result = document.getElementById('solutionWindow').value
- buttons.forEach(function(button) {button.addEventListener("click", function() {solutionWindow.value += button.value})});
- document.querySelector('#clearButton').addEventListener("click", function() {solutionWindow.value = (" ");});
- document.querySelector('#equal').addEventListener("click", calculate);
+buttons.forEach(function(button){button.addEventListener('click', function() {(solutionWindow.value += button.value)})});
+clear.addEventListener('click', function(){solutionWindow.value = " "});
+equal.addEventListener('click', calculate)
